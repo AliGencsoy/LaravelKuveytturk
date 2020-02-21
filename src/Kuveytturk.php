79a0ef5deb165b55d4cbe64e595ebb56a7d91164
Ali Gençsoy <agencsoy65@gmail.com>
2020-02-21 20:40:08 +0300

    remove debug codes

--- src/Kuveytturk.php
+++ src/Kuveytturk.php
@@ -218,17 +218,4 @@ EOT;
 
 		return $xml->__toString();
 	}
-
-	public function getUserIpAddr() {
-	    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-	        //ip from share internet
-	        $ip = $_SERVER['HTTP_CLIENT_IP'];
-	    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-	        //ip pass from proxy
-	        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-	    }else{
-	        $ip = $_SERVER['REMOTE_ADDR'];
-	    }
-	    return $ip;
-	}
 }
